04b20fc5d97d3b4f09042d278c9354847a110bde
morikentiger <kenrur339@gmail.com>
2021-01-26 10:59:18 +0900

    ・石を置くメソッド・ひっくりかえせる石の数を列挙するメソッド

--- reversi.py
+++ reversi.py
@@ -1,3 +1,6 @@
+# 参考URL
+# https://katoh4u.hatenablog.com/entry/2018/03/22/130105
+
 WHITE = 0
 BLACK = 1
 BOARD_SIZE = 8
@@ -17,5 +20,62 @@ class ReversiBoard(object):
 		self.cells[4][4] = WHITE
 
 
-board = ReversiBoard()
-print(*board.cells, sep='\n')
+		board = ReversiBoard()
+		print(*board.cells, sep='\n')
+
+	def put_disk(self, x, y, player):
+		# すでに他の石があれば置くことができない
+		if self.cells[y][x] is not None:
+			return False
+
+		# 獲得できる石がない場合も置くことができない
+		flippable = self.list_flippable_disks(x, y, player)
+		if flippable == []:
+			return False
+
+		# 実際に石を置く処理
+		self.cells[y][x] = player
+		for x,y in flippable:
+			self.cells[y][x] = player
+
+		return True
+
+	def list_flippable_disks(self, x, y, player):
+		PREV = -1
+		NEXT = 1
+		DIRECTION = [PREV, 0, NEXT]
+		flippable = []
+
+		for dx in DIRECTION:
+			for dy in DIRECTION:
+				if dx == 0 and dy == 0:
+					continue
+
+				tmp = []
+				depth = 0
+				while(True):
+					depth += 1
+
+					# 方向 x 深さ（距離）を要求座標に加算し直線的な探査をする
+					rx = x + (dx * depth)
+					ry = y + (dy * depth)
+
+					# 調べる座標(rx, ry)がボードの範囲内ならば
+					if 0 <= rx < BOARD_SIZE and 0 <= ry < BOARD_SIZE:
+						request = self.cells[ry][rx]
+
+						# Noneを獲得することはできない
+						if request is None:
+							break
+
+						if request == player:	# 自分の石が見つかったとき
+							if tmp != []:		# 探査した範囲内に獲得可能な石があれば
+								flippable.extend(tmp)	# flippableに追加
+
+						# 相手の石が見つかったとき
+						else:
+							# 獲得可能な石として一時保存
+							tmp.append((rx, ry))
+					else:
+						break		
+		return flippable
